93207c41c99420e3c318b8d95a2ddb9868e0a7f8
ErikBeelen <eriklbeelen@gmail.com>
2025-10-09 15:24:29 +0200

    uitwerking

---
+++ Bootstrap/bootstrap.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Bootstrap pagina</title>
+</head>
+<body>
+    <div class="container">
+        <?php include 'includes/navbar.php'; ?>
+        <?php include 'includes/header.php'; ?>
+            <main class="row mt-3">
+                <div class="col-md-4 bg-light p-5">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                </div>
+                <div class="col-md-4 bg-light p-5">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                </div>
+                <div class="col-md-4 bg-light p-5">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                </div>
+            </main>
+    </div>
+</body>
+</html>
