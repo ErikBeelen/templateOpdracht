e78757238db651983a4aae437833efe8977573c1
ErikBeelen <eriklbeelen@gmail.com>
2025-10-10 10:09:23 +0200

    includes

---
+++ Bootstrap/includes/header.php
@@ -0,0 +1,5 @@
+<header class="row mt-3 bg-light">    
+                <div class="col-md-12 bg-light p-5">
+                    <p class="h1">Hello, world</p>
+                </div>
+            </header>
---
+++ Bootstrap/includes/navbar.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-sm bg-light mt-2">
+            <div class="container-fluid">
+                <!-- Links -->
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="bootstrap.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                </ul>
+            </nav>
