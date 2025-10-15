9d48b6e04bb81d92df443ec27ff7c0358268aa1e
ErikBeelen <eriklbeelen@gmail.com>
2025-10-15 14:57:36 +0200

    query bestand toegevoegt

---
+++ mysql/queries.sql
@@ -0,0 +1,11 @@
+
+Select * FROM customers;
+
+Select * FROM customers WHERE country = 'France';
+
+Select * FROM customers ASC;
+
+Select * FROM employees WHERE buyPrice < 50;
+
+Select * FROM products WHERE productLine = 'Classic Cars';
+
